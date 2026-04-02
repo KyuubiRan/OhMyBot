8b6f1e14e67e6bfad77711fd1b31bba49823d92c
Kitsune <KyuubiRan@outlook.com>
2026-04-02 16:07:48 +0800

    feat: add ChatType

---
+++ OhMyOneBot.V11.Lib/src/Enums/ChatType.cs
@@ -0,0 +1,7 @@
+namespace OhMyOneBot.V11.Lib.Enums;
+
+public enum ChatType
+{
+    Private,
+    Group
+}
--- OhMyOneBot.V11.Lib/src/Messages/MessageChain.cs
+++ OhMyOneBot.V11.Lib/src/Messages/MessageChain.cs
@@ -1,3 +1,4 @@
+using OhMyOneBot.V11.Lib.Enums;
 using OhMyOneBot.V11.Lib.Messages.Entity;
 
 namespace OhMyOneBot.V11.Lib.Messages;
@@ -7,5 +8,6 @@ public class MessageChain
     public required string ChatId { get; init; }
     public required string SenderId { get; init; }
     public required string MessageId { get; init; }
+    public required ChatType ChatType { get; init; }
     public List<MessageEntity> Entities { get; init; } = [];
 }
